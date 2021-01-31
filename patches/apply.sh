ac7a4f2594d8235ab6842a32d3b4ee3d232cf26f
Yasuaki Uechi <y@uechi.io>
2021-01-31 12:35:05 +0900

    chore: update env

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-patch -ruN -p0 -d ./data/app/lib < patches/lib.patch
-patch -ruN -p0 -d ./data/app/resources < patches/resources.patch
+patch --binary -ruN -p0 -d ./data/app/lib < patches/lib.patch
+patch --binary -ruN -p0 -d ./data/app/resources < patches/resources.patch
